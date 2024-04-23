42bf1af7aab7f7c41d7869fc5600bd00ba62abc3
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2024-04-23 12:05:49 +0700

    test cors for fe server

--- src/chat/socket-oi-adapter.ts
+++ src/chat/socket-oi-adapter.ts
@@ -20,7 +20,7 @@ export class SocketIOAdapter extends IoAdapter {
 
     const cors = {
       origin: [
-        `${clientUrl}:${clientPort}`,
+        `http://ec2-47-129-30-6.ap-southeast-1.compute.amazonaws.com/`,
         new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
       ],
     };
