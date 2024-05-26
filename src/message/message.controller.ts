20e8bbf9c865d36ce95681e8664581cca3138e3e
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2024-05-26 12:57:08 +0700

    refactor(notification): add isViewed

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { MyJwtGuard } from '../auth/guard/myjwt.guard';
 import { MessageService } from './message.service';
 
@@ -10,5 +18,11 @@ export class MessageController {
   @UseGuards(MyJwtGuard)
   getMessage(@Req() req, @Query('chatRoomId') chatRoomId: string) {
     return this.messageService.getMessage(req.user, chatRoomId);
+  }
+
+  @UseGuards(MyJwtGuard)
+  @Post('view')
+  async updateMessage(@Req() req, @Body() messages: { ids: string[] }) {
+    return this.messageService.viewMessages(messages.ids);
   }
 }
